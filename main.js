80cb630266e9fe432e9edc7685ea8f3b56324f8e
AlexaJosse <=>
2020-09-04 21:24:52 +0200

    Adds a filter to the user extraction. Modifies the manip object :the date is formatted.

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const onOpen = () => {
     .addToUi();
 }
 
-const include = (filename) =>{
+const include = (filename) => {
   return HtmlService.createHtmlOutputFromFile(filename)
-      .getContent();
+    .getContent();
 }
 
 const displaySidebarForm = () => {
@@ -20,23 +20,24 @@ const displaySidebarForm = () => {
 const getUsers = (mainSheet) => {
   return mainSheet.getRange(2, 1, mainSheet.getLastRow() - 1, 4)
     .getValues()
-    .map((userRow) => {
-      return {
-        lastName: userRow[0],
-        firstName: userRow[1],
-        nickName: userRow[2],
-        number: userRow[3],
-      }
-    });
-}
+    .filter((userRow) => (userRow[0] && userRow[3]))
+    .map((userRow) => ({
+      lastName: userRow[0],
+      firstName: userRow[1],
+      nickName: userRow[2],
+      number: userRow[3],
+    })
+  )
+};
 
 const getManips = (manipSheet) => {
   return manipSheet.getRange(2, 1, manipSheet.getLastRow() - 1, 3)
-    .getValues()
+    .getDisplayValues()
     .map((manipRow) => {
+      const dateData = manipRow[1].split('/').map((obj) => parseInt(obj));
       return {
-        name: manipRow[0],
-        date: manipRow[1]
+        'name': manipRow[0],
+        'date': new Date(dateData[2], dateData[1] - 1, dateData[0])
       }
     });
 }
@@ -44,7 +45,7 @@ const getManips = (manipSheet) => {
 const onSidebarLoaded = () => {
   const mainSpreadsheet = SpreadsheetApp.getActive();
   return JSON.stringify({
-    "userArray" :getUsers(mainSpreadsheet.getSheetByName(MAIN_SHEET)) ,
-    "manipArray":getManips(mainSpreadsheet.getSheetByName(MANIP_SHEET))
+    "userArray": getUsers(mainSpreadsheet.getSheetByName(MAIN_SHEET)),
+    "manipArray": getManips(mainSpreadsheet.getSheetByName(MANIP_SHEET))
   })
 }
